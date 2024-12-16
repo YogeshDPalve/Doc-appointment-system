d52cb872c5d9422c27ba8a37c72fd6dbefb624bf
Yogesh Palve <dnyyogesh@gmail.com>
2024-12-16 17:52:29 +0530

    user login complete

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,20 +1,34 @@
 import React from 'react'
-import { Form, Input } from 'antd'
+import { Form, Input, message } from 'antd'
 import '../styles/RegisterStyles.css'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import axios from 'axios';
+axios.defaults.baseURL = 'http://localhost:4000';
+
 const Login = () => {
+  const navigate = useNavigate();
   // form handler
-  const onFinishHandler = (values) => {
-    console.log(values)
+  const onFinishHandler = async (values) => {
+    // console.log(values)
+    try {
+      const res = await axios.post('api/v1/user/login', values);
+      if (res.data.success) {
+        localStorage.setItem("token", res.data.token);
+        message.success('Login Successfull !');
+        navigate('/');
+      } else {
+        message.error(res.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      message.error('Something went wrong');
+    }
   }
   return (
     <>
       <div className='form-container'>
         <Form layout='vertical' onFinish={onFinishHandler} className='  register-form'>
           <h1 className='text-center'>Login Form</h1>
-          <Form.Item label='Name' name='name' >
-            <Input type='text' required />
-          </Form.Item>
           <Form.Item label='Email' name='email' >
             <Input type='email' required />
           </Form.Item>
